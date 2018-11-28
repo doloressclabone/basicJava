2be13c3464c79a9a22841d163807a39ae400495a
PC02 <PC02@PC02-PC>
2018-11-28 19:23:21 +0900

    초급자바 최초 commit 실패 이후 retry

--- src/Y_exam/Programmers.java
+++ src/Y_exam/Programmers.java
@@ -3,3 +3,15 @@ package Y_exam;
 public class Programmers {
 
 }
+
+class Solution{
+	  public String solution(int n) {
+	      if(n>0){
+	    	for(int i=1; i<=n; i++){
+	    		System.out.println();
+	    	}
+	      }
+	      String answer = "";
+	      return answer;
+	  }
+}
